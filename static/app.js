98b190d557d2f3846b9fd892828659f3babb2507
sofiaabrahamsen <sofia.abrahamsen@yahoo.com>
2024-12-11 17:22:20 +0100

    hardcode restaurantLocations med user_pk (men nu kan man klikke på en markør på mappet og komme hen til dens single side)

--- static/app.js
+++ static/app.js
@@ -1,8 +1,12 @@
-function render_items(data){
-    data = JSON.parse(data)
-    data.forEach(e=>{
-        console.log(e)
-        var marker = L.marker(e.coords).addTo(map)
-        marker.bindPopup(e.popup)
-    })
+function render_items(data) {
+    data = JSON.parse(data);
+    data.forEach(e => {
+        // Create the marker
+        var marker = L.marker(e.coords).addTo(map);
+
+        // Bind the popup with the restaurant's name and a link to the customer-single page
+        marker.bindPopup(`
+            <a href="/customer-single/${e.user_pk}">${e.name}</a>
+        `);
+    });
 }
